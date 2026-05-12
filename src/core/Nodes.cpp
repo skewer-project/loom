cba2adcd07eee9455d32503f5f5eee1aeacb4416
Ian Yoo <isyoo@ucdavis.edu>
2026-05-11 23:07:18 -0700

    Implement DeepReadNode

--- src/core/Nodes.cpp
+++ src/core/Nodes.cpp
@@ -1,15 +1,19 @@
-#include "core/Nodes.hpp"
+#include "core/Nodes.cpp"
 
 #include <cassert>
 #include <cstring>
 #include <iostream>
 
+#include "core/DeepExrLoader.hpp"
 #include "core/EvaluationContext.hpp"
 #include "core/Graph.hpp"
 #include "core/RenderCache.hpp"
+#include "gpu/BindlessHeap.hpp"
 #include "gpu/ComputeTask.hpp"
 #include "gpu/PipelineCache.hpp"
+#include "gpu/TransientBufferPool.hpp"
 #include "gpu/TransientImagePool.hpp"
+#include "gpu/VulkanContext.hpp"
 
 namespace loom::core {
 
